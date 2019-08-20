aeb64470935e8eaaf0fd831490d1d0286905621e
panqiang <1126525692@qq.com>
2019-08-20 20:24:42 +0800

    format

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
-
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
+// vs code format: Shift + Alt + F
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.sync.set({ color: '#3aa757' }, function () {
         console.log("The color is green.");
     });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [
                 new chrome.declarativeContent.PageStateMatcher({
-                    pageUrl: {hostEquals: 'www.baidu.com'},
+                    pageUrl: { hostEquals: 'www.baidu.com' },
                 })
             ],
             actions: [
