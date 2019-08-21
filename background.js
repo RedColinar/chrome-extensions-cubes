233ec6b040058a9e754e7408b775b96e0b79ed54
panqiang <1126525692@qq.com>
2019-08-21 09:19:06 +0800

    save and retrive data from chrome

--- background.js
+++ background.js
@@ -1,8 +1,5 @@
 // vs code format: Shift + Alt + F
 chrome.runtime.onInstalled.addListener(function () {
-    chrome.storage.sync.set({ color: '#3aa757' }, function () {
-        console.log("The color is green.");
-    });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [
@@ -15,4 +12,7 @@ chrome.runtime.onInstalled.addListener(function () {
             ]
         }]);
     });
+    // chrome.webNavigation.onCompleted.addListener(function () {
+    //     alert("webNavigation.onCompleted")
+    // }, { url: { urlMatches: '<all_urls>' } })
 });
--- options.js
+++ options.js
@@ -16,9 +16,25 @@ function constructOptions(kButtonColors) {
 }
 constructOptions(kButtonColors);
 
-function saveOptions() {
+chrome.storage.sync.get(['url'], function (result) {
+    console.log('get url is ' + result.url);
+    document.getElementById('url-expected-1').value = result.url
+})
+chrome.storage.sync.get(['js'], function (result) {
+    console.log('get js is ' + result.js);
+    document.getElementById('js-expected-1').value = result.js
+})
 
+function saveOptions() {
+    let url = document.getElementById('url-expected-1').value
+    let js = document.getElementById('js-expected-1').value
+    chrome.storage.sync.set({ 'url': url }, function () {
+        console.log('save url is ' + url);
+    })
+    chrome.storage.sync.set({ 'js': js }, function () {
+        console.log('save js is ' + js);
+    })
 }
 saveButton.addEventListener('click', function () {
-    
+    saveOptions()
 })
